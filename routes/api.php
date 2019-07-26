a4b5f6e2249362f4c21bb31b0bb7cef6d2e8501d
Miguel Rea <miguel@m22.mx>
2019-07-26 02:08:59 -0500

    Creating end-point for the RESTful API Using laravel Resource Routes

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,9 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::resource('students', 'Students\StudentsController');
+Route::resource('subjects', 'Subjects\SubjectsController');
+Route::resource('scores', 'Scores\ScoresController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
